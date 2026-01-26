f0b77074fdc3ccf3585edcb3e123482aafe05b88
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-01-25 23:27:15 -0300

    Add basic function examples and event listener
    Introduces several example functions: areaQuadrado, pi, imc, and corFavorita, each demonstrating different JavaScript concepts. Also adds a click event listener that logs a message to the console.

---
+++ Origamid/funcoes/funcoes.js
@@ -0,0 +1,45 @@
+/* Bloco de código que pode ser executado e reutilizado. Valores podem ser passados por uma função e a mesma retorna outro valor. */
+
+
+
+function areaQuadrado(lado) {
+  return lado * lado;
+}  
+
+console.log(areaQuadrado(4)); // 16
+
+
+function pi() {
+    return 3.14;
+}
+
+let valorTotal = 5 * pi(); // 15.7
+console.log(valorTotal);
+
+
+
+
+function imc(peso, altura) {
+    let imc = peso / (altura ** 2);
+    return imc;
+}
+
+console.log(imc(80, 1.8)); // 24.69...
+
+
+function corFavorita(cor) {
+    if(cor === 'azul') {
+        return 'Eu gosto do céu';
+    } else if(cor === 'verde') {
+        return 'Eu gosto de mato';
+    } else {
+        return 'Eu não gosto de cores';
+    }
+}
+
+corFavorita('azul'); // 'Eu gosto do céu'
+
+
+addEventListener('click', function(){
+    console.log('oi');
+});
