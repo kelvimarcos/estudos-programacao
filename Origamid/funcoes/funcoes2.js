e0f4264589e40dccf92fe8e60d2a15c2e91cf03f
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-01-26 22:31:02 -0300

    Add functions for IMC calculation and age check
    Introduced imc2 to calculate and log BMI, and terceiraIdade to check and log if age qualifies as 'terceira idade'. Includes example calls for both functions.

---
+++ Origamid/funcoes/funcoes2.js
@@ -0,0 +1,15 @@
+function imc2(peso, altura){
+    const imc = peso / (altura ** 2);
+    console.log(imc);
+}
+
+imc2(20, 1.8); // 24.69
+
+
+function terceiraIdade(idade){
+    if(idade >= 60){
+        console.log('Terceira idade');
+    } else {
+        console.log('Não é terceira idade');
+    }}
+terceiraIdade(20); // Terceira idade
