7883d0eb505f9844a496288cfefd30828b547b6b
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-01-30 16:49:02 -0300

    Add basic DOM example with button and script
    Created index.html with a button and linked script.js. Added script.js to demonstrate event handling, string methods, and DOM element properties for learning purposes.

---
+++ Origamid/DOM/script.js
@@ -0,0 +1,36 @@
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function() {
+    document.writeln('Clicou' );
+});
+
+
+
+//nomeie 3 propriedades ou métodos de Strings
+
+let nome = 'Kelvison';
+/* fixed
+tolowerCase
+trim
+length */
+
+
+
+//nomeie 5 propriedades ou métodos de elementos do DOM
+
+let btn = document.querySelector('.btn');
+
+/* addEventListener
+id
+innerHtml
+classList
+value */
+
+
+//busca um metodo na web que é capaz de interagir com o clipboard (area de transferencia do sistema)
+
+
+
+
+
+
