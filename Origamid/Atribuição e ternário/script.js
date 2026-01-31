3d19c2f85ed5b786ed3c13538df14a36a9b6aba2
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-01-31 07:07:27 -0300

    Add examples of assignment and ternary operators
    Created index.html and script.js demonstrating assignment operators in JavaScript. The script shows usage of +=, -=, *=, and /= with console output.

---
+++ Origamid/Atribuição e ternário/script.js
@@ -0,0 +1,19 @@
+
+// Atribuição e ternário
+
+
+let numero = 20
+let numero2 = 40
+
+numero += numero2; // numero = numero + 10
+console.log(numero);
+
+numero -= numero2; // numero = numero - 10
+console.log(numero);
+
+numero *= numero2; // numero = numero * 10
+console.log(numero);      
+
+numero /= numero2; // numero = numero / 10
+console.log(numero);
+
