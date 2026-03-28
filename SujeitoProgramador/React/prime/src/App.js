1c2ff9acc99bcdd302475a22816240b43310ac6d
Kelvison Marcos <kelvison.marcos10@gmail.com>
2026-03-28 18:30:59 -0300

    Constructor Function

---
+++ SujeitoProgramador/React/prime/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      
+
+<h1>teste</h1>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ SujeitoProgramador/React/prime/src/pages/routes.js
@@ -0,0 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+import Home from "./home";
+import Filme from "./Filme";
+
+
+
+
+function RoutesApp() {
+    return (
+        <BrowserRouter>
+
+        <routes>
+
+
+
+        </routes>
+           
+        </BrowserRouter>
+    );
+
+}
+
+export default RoutesApp;
